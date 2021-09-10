2cf5f9408acb27b8eefd05547003a4b0bf00a26b
Richard <richard.yang.tong@gmail.com>
2021-09-10 11:29:42 -0400

    Smalled fixes.

--- c5_process_API/ex1_fork.c
+++ c5_process_API/ex1_fork.c
@@ -14,7 +14,7 @@ int main(int argc, char* argv[])
     } else if (rc == 0) { // child(new process)
         printf("hello, I am child(pid:%d)\n", (int) getpid());
     } else { // parent goes down this path(main)
-        printf("hello, I am parent %d (pid:%d)\n", rc, (int) getpid());
+        printf("hello, I am parent of %d (pid:%d)\n", rc, (int) getpid());
     }
 
     return 0;
